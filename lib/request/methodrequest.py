20a7c66fdb20b6194578a1a292ad8d8b17391e08
GermanAizek <GermanAizek@yandex.ru>
2018-06-06 15:00:05 +0300

    Using 2to3 tool

---
+++ lib/request/methodrequest.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+"""
+Copyright (c) 2006-2018 sqlmap developers (http://sqlmap.org/)
+See the file 'LICENSE' for copying permission
+"""
+
+import urllib.request, urllib.error, urllib.parse
+
+class MethodRequest(urllib.request.Request):
+    """
+    Used to create HEAD/PUT/DELETE/... requests with urllib2
+    """
+
+    def set_method(self, method):
+        self.method = method.upper()
+
+    def get_method(self):
+        return getattr(self, 'method', urllib.request.Request.get_method(self))
