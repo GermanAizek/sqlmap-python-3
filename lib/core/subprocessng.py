1ca28419dc3c8e7bead366568236e590706519ee
GermanAizek <GermanAizek@yandex.ru>
2019-04-18 18:27:15 +0300

    [FIX] Unused variable 'errCode', 'nMessage'

--- lib/core/subprocessng.py
+++ lib/core/subprocessng.py
@@ -92,7 +92,7 @@ class Popen(subprocess.Popen):
 
             try:
                 x = msvcrt.get_osfhandle(self.stdin.fileno())
-                (errCode, written) = WriteFile(x, input)
+                (_, written) = WriteFile(x, input)
             except ValueError:
                 return self._close('stdin')
             except (subprocess.pywintypes.error, Exception) as why:
@@ -109,11 +109,11 @@ class Popen(subprocess.Popen):
 
             try:
                 x = msvcrt.get_osfhandle(conn.fileno())
-                (read, nAvail, nMessage) = PeekNamedPipe(x, 0)
+                (read, nAvail, _) = PeekNamedPipe(x, 0)
                 if maxsize < nAvail:
                     nAvail = maxsize
                 if nAvail > 0:
-                    (errCode, read) = ReadFile(x, nAvail, None)
+                    (_, read) = ReadFile(x, nAvail, None)
             except (ValueError, NameError):
                 return self._close(which)
             except (subprocess.pywintypes.error, Exception) as why:
